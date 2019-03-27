fbbc0590f54671c4f99b77400654241e91e05e08
vishangshah <shahvishang@gmail.com>
2019-03-27 16:17:48 -0400

    Trying out super method.

--- Python3-OOP-Book/ch-03/inheritance/contact.py
+++ Python3-OOP-Book/ch-03/inheritance/contact.py
@@ -21,6 +21,10 @@ class Contact:
         self.email = email
         Contact.all_contacts.append(self)
 
+class Friend(Contact):
+    def __init__(self, name, email, phone):
+        super().__init__(name, email)
+        self.phone = phone
 
 class Supplier(Contact):
     def order(self, order):
